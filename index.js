a378c3a026ff09230f3c2f574c4e1963feb7481e
kalille Overton <>
2021-04-10 09:41:18 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
+let num1 = 31;
+let num2 = 2;
+let multiply = (num1 * num2);
+let random= (Math.ceil(Math.random()) * 10);
+let num3 = 40;
+let num4 = 9;
+let mod = (num3 % num4 );
+let max = Math.max(5,10,15,20) 
 // Write your code here
